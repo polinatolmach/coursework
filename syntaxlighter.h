01935c2b03a84ac814551c636126a8d5a986cb81
polinatolmach <polina.tolmach@gmail.com>
2015-05-31 14:40:26 +0300

    Create syntaxlighter.h

---
+++ syntaxlighter.h
@@ -0,0 +1,45 @@
+#ifndef SYNTAXLIGHTER_H
+#define SYNTAXLIGHTER_H
+
+#include <QtGui>
+#include <QTextDocument>
+#include <QHash>
+#include <QRegExp>
+#include <QTextCharFormat>
+
+class SyntaxLighter : public QSyntaxHighlighter
+{
+    Q_OBJECT
+
+public:
+ SyntaxLighter(QTextDocument *parent = 0);
+
+protected:
+
+void highlightBlock(const QString &text);
+
+
+private:
+    struct HighlightingRule
+       {
+           QRegExp pattern;
+           QTextCharFormat format;
+       };
+       QVector<HighlightingRule> highlightingRules;
+
+       QRegExp commentStartExpression;
+       QRegExp commentEndExpression;
+
+       QTextCharFormat keywordFormat;
+       QTextCharFormat classFormat;
+       QTextCharFormat singleLineCommentFormat;
+       QTextCharFormat multiLineCommentFormat;
+       QTextCharFormat quotationFormat;
+       QTextCharFormat functionFormat;
+       QTextCharFormat numberFormat;
+   };
+
+
+
+
+#endif
