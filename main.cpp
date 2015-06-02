b6134a5fab5f5aec8cf3d953cfa4f84fc9b956e9
polinatolmach <polina.tolmach@gmail.com>
2015-06-03 02:30:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-   
+    MainWindow window;
+    window.resize(640, 512);
+    window.show();
+
     return a.exec();
 }
