1de961fc81df7f3879d254319156053eda40b92b
polinatolmach <polina.tolmach@gmail.com>
2015-06-03 02:27:58 +0300

    updared mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "syntaxlighter.h"
+#include "codeeditor.h"
 
 #include <QMessageBox>
 #include <QVector>
-
+#include <QFileInfo>
+#include <QString>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -13,47 +15,14 @@ MainWindow::MainWindow(QWidget *parent) :
 {
 
     ui->setupUi(this);
+    setupEditor();
 
-
-    this->setFixedHeight(600);
-    this->setFixedWidth(800);
-    this->setWindowTitle("pEdit 1.0");
+    this->setWindowTitle("Untitled - pEdit 1.0");
     this->setAcceptDrops(true);
     this->lightfactor = 120;
 
     setCentralWidget(ui->tabWidget);
-    tabs.resize(1);
 
-    MainWindow::setWindowTitle("Untitled - pEdit");
-    ui->plainTextEdit->setAcceptDrops(FALSE);
-     MainWindow::setWindowTitle("Untitled - pEdit");
- //    MainWindow::setWindowIcon(QIcon(QApplication::applicationDirPath() + "/icon.png"));
-     ui->tabWidget->setTabsClosable(false);
-
-     Tab tab;
-     tab.number = 0;
-     tab.path = "";
-     tab.filename = "";
-     tab.plainTextEdit = ui->plainTextEdit;
-     tab.highlighter = new SyntaxLighter(ui->plainTextEdit->document());
-     tabs.push_back(tab);
-    //sets the tab data for the first tab
-     ui->tabWidget->setTabText(0, "Untitled");
-     ui->plainTextEdit->setPlainText("Yo.");
-
-
-
-     QFont font;
-    font.setFamily("Courier");
-    font.setStyleHint(QFont::Monospace);
-    font.setFixedPitch(true);
-    font.setPointSize(10);
-
-    ui->plainTextEdit->setFont(font);
-    const int tabStop = 4;  // 4 characters
-
-    QFontMetrics metrics(font);
-    ui->plainTextEdit->setTabStopWidth(tabStop * metrics.width(' '));
 
     QToolButton *newTabButton = new QToolButton(this);
     ui->tabWidget->setCornerWidget(newTabButton);
@@ -62,14 +31,62 @@ MainWindow::MainWindow(QWidget *parent) :
      //connect signals
     QObject::connect(newTabButton, SIGNAL(clicked()), this, SLOT(makeNewTab()));
     QObject::connect(ui->tabWidget, SIGNAL(tabCloseRequested(int)), this, SLOT(deleteTab(int)));
-    QObject::connect(this, SIGNAL(cursorPositionChanged(textCursor)), this, SLOT(highlightCurrentLine()));
-    highlightCurrentLine();
-
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setupEditor()
+{
+tabs.clear();
+tabs.resize(1);
+MainWindow::setWindowTitle("Untitled - pEdit");
+
+ MainWindow::setWindowTitle("Untitled - pEdit");
+ MainWindow::setWindowIcon(QIcon(QApplication::applicationDirPath() + "/text-editor.png"));
+ ui->tabWidget->setTabsClosable(false);
+tabs.clear();
+
+Tab *tab = new Tab();
+tabs.push_back(tab);
+tab->editor = new QCodeEditor();
+tab->highlighter = new SyntaxLighter(tab->editor->document());
+ui->tabWidget->addTab(tab->editor, "Untitled");
+tab->number = 0;//tabs.back()->number + 1;
+tab->path = "";
+tab->filename = "";
+ui->tabWidget->setTabText(0, "Untitled");
+
+}
+
+
+
+QString MainWindow::extractFilename(QString path)
+{
+    std::string stdPath = path.toStdString();
+  //  #ifdef WIN32
+  //      return QString::fromStdString(stdPath.substr(stdPath.find_last_of('\\') + 1));
+  //  #else
+       return QString::fromStdString(stdPath.substr(stdPath.find_last_of('/') + 1));
+  //  #endif
+}
+
+
+void MainWindow::openFile()
+{
+    QString tmpPath = QFileDialog::getOpenFileName(this);
+    QFile file(tmpPath);
+    if(file.open(QIODevice::ReadOnly|QIODevice::Text)) {
+        tabs[currentTab()]->path = tmpPath;
+        QByteArray byteArray = file.readAll();
+        tabs[currentTab()]->editor->setPlainText(byteArray.data());
+
+        MainWindow::setWindowTitle(tabs[currentTab()]->path.toUtf8() + " - pEdit");
+        tabs[currentTab()]->filename = extractFilename(tabs[currentTab()]->path);
+        setTabName(currentTab(), tabs[currentTab()]->filename);
+    }
 }
 
 
@@ -79,68 +96,70 @@ int MainWindow::currentTab()
 
 }
 
+void MainWindow::saveFileAs()
+{
+    QString tempPath = QFileDialog::getSaveFileName();
+    QFile file(tempPath);
+   if (file.open(QIODevice::WriteOnly|QIODevice::Text)) {
+        tabs[currentTab()]->path = tempPath;
+        file.write(tabs[currentTab()]->editor->toPlainText().toUtf8());
+        MainWindow::setWindowTitle(tabs[currentTab()]->path.toUtf8() + " - pEdit");
+        tabs[currentTab()]->filename = extractFilename(tabs[currentTab()]->path);
+        setTabName(currentTab(), tabs[currentTab()]->filename);
+    }
+}
 
-//void MainWindow::openFile()
-//{
-//    QString tmpPath = QFileDialog::getOpenFileName(this);
-//    QFile file(tmpPath);
-//    if(file.open(QIODevice::ReadOnly|QIODevice::Text)) {
-//        tabs[currentTab()].path = tmpPath;
-//        QByteArray byteArray = file.readAll();
-//        tabs[currentTab()].plainTextEdit->setPlainText(byteArray.data());
+void MainWindow::switchToNextTab()
+{
+    if (currentTab() != (tabs.size() - 1)) {
+        ui->tabWidget->setCurrentIndex(currentTab() + 1);
+    }else {
+        ui->tabWidget->setCurrentIndex(0);
+    }
+}
 
-     //   MainWindow::setWindowTitle(tabs[currentTab()].path.toUtf8() + " - BenPad");
-//        tabs[currentTab()].filename = extractFilename(tabs[currentTab()].path);
-//        setTabName(currentTab(), tabs[currentTab()].filename);
+void MainWindow::switchToPrevTab()
+{
+    if (currentTab() != 0) {
+        ui->tabWidget->setCurrentIndex(currentTab() - 1);
+    }
+    else {
+        ui->tabWidget->setCurrentIndex(tabs.size() - 1);
+    }
+}
 
+void MainWindow::saveFile()
+{
+    QFile file(tabs[currentTab()]->path);
+    if (file.open(QIODevice::WriteOnly|QIODevice::Text)) {
+        file.write(tabs[currentTab()]->editor->toPlainText().toUtf8());
+    }
+}
 
+void MainWindow::on_actionOpen_triggered()
+{
+  openFile();
+}
 
+void MainWindow::on_actionSave_triggered()
+{
+  if (tabs[currentTab()]->path.isEmpty())
+  {
+      saveFileAs();
+  }else {
+      saveFile();
+  }
+}
 
-//void MainWindow::saveFileAs()
-//{
-//    QString tmpPath = QFileDialog::getSaveFileName();
-//    QFile file(tmpPath);
-//    if (file.open(QIODevice::WriteOnly|QIODevice::Text)) {
-//        tabs[currentTab()].path = tmpPath;
-//        file.write(ui->plainTextEdit->toPlainText().toUtf8());
-//        MainWindow::setWindowTitle(tabs[currentTab()].path.toUtf8() + " - BenPad");
-//      //  tabs[currentTab()].filename = extractFilename(tabs[currentTab()].path);
-//        setTabName(currentTab(), tabs[currentTab()].filename);
-//    }
-//}
-
-//void MainWindow::saveFile()
-//{
-//    QFile file(tabs[currentTab()].path);
-//    if (file.open(QIODevice::WriteOnly|QIODevice::Text)) {
-//        file.write(ui->plainTextEdit->toPlainText().toUtf8());
-//    }
-//}
-
-//void MainWindow::on_actionOpen_triggered()
-//{
-//  openFile();
-//}
-
-//void MainWindow::on_actionSave_triggered()
-//{
-//  if (tabs[currentTab()].path.isEmpty())
-//  {
-//      saveFileAs();
-//  }else {
-//      saveFile();
-//  }
-//}
-
-//void MainWindow::on_actionSave_As_triggered()
-//{
-//    saveFileAs();
-//}
+void MainWindow::on_actionSave_As_triggered()
+{
+    saveFileAs();
+}
 
 
 void MainWindow::on_actionNew_triggered()
 {
-    ui->plainTextEdit->clear();
+    tabs[currentTab()]->editor->clear();
     MainWindow::setWindowTitle("Untitled - pEdit 1.0");
     setTabName(currentTab(), "Untitled");
 }
@@ -148,24 +167,23 @@ void MainWindow::on_actionNew_triggered()
 void MainWindow::makeNewTab()
 {
 
-    tabs.resize(tabs.size()+1);
-    Tab tab;
-    tab.plainTextEdit = new QPlainTextEdit;
-    tab.highlighter = new SyntaxLighter(ui->plainTextEdit->document());
-    ui->tabWidget->addTab(tab.plainTextEdit, "Untitled");
-    tab.number = tabs[tabs.size() - 1].number + 1;
-    tab.path = "";
-    tab.filename = "";
+   // tabs.resize(tabs.size()+1);
+    Tab *tab = new Tab();
+    tab->editor = new QCodeEditor();
+    tab->highlighter = new SyntaxLighter(tab->editor->document());
+    ui->tabWidget->addTab(tab->editor, "Untitled");
+    tab->number = tabs.back()->number + 1;
+    tab->path = "";
+    tab->filename = "";
     tabs.push_back(tab);
 
-    ui->tabWidget->setCurrentIndex(tab.number);
+
+    ui->tabWidget->setCurrentIndex(tab->number);
 
     if (tabs.size() > 1) {
         ui->tabWidget->setTabsClosable(true);
     }
 }
-
-
 
 
 void MainWindow::on_actionExit_triggered()
@@ -177,22 +195,9 @@ void MainWindow::on_actionExit_triggered()
 void MainWindow::on_actionAbout_triggered()
 {
      QMessageBox::about(this,tr("About"), tr("<h1>Help</h1>Here you can get some help.</br> And information."));
-
-
 }
 
-  void MainWindow::highlightCurrentLine()
- {
 
-     QTextEdit::ExtraSelection highlight;
-     highlight.cursor = ui->plainTextEdit->textCursor();
-     highlight.format.setProperty(QTextFormat::FullWidthSelection, true);
-     highlight.format.setBackground(Qt::lightGray);
-
-     QList<QTextEdit::ExtraSelection> extras;
-     extras << highlight;
-     ui->plainTextEdit->setExtraSelections( extras );
- }
 
   void MainWindow::setTabName(unsigned int index, QString text){
       ui->tabWidget->setTabText(index, text);
@@ -202,36 +207,35 @@ void MainWindow::on_actionAbout_triggered()
 void MainWindow::on_actionNew_tab_triggered()
 {
     makeNewTab();
+
 }
 
 
 void MainWindow::on_actionCopy_triggered()
 {
-   tabs[currentTab()].plainTextEdit->copy();
+   tabs[currentTab()]->editor->copy();
 }
 
 void MainWindow::on_actionPaste_triggered()
 {
-   tabs[currentTab()].plainTextEdit->paste();
-
+   tabs[currentTab()]->editor->paste();
 }
 
 void MainWindow::on_actionUndo_triggered()
 {
-  tabs[currentTab()].plainTextEdit->undo();
+  tabs[currentTab()]->editor->undo();
 
 }
 
 void MainWindow::on_actionRedo_triggered()
 {
-   tabs[currentTab()].plainTextEdit->redo();
+   tabs[currentTab()]->editor->redo();
 
 }
 
 void MainWindow::on_actionCut_triggered()
 {
-   tabs[currentTab()].plainTextEdit->cut();
-
+   tabs[currentTab()]->editor->cut();
 }
 
 void MainWindow::deleteCurrentTab()
@@ -241,12 +245,16 @@ void MainWindow::deleteCurrentTab()
 
 void MainWindow::deleteTab(int i)
 {
-    tabs[currentTab()].plainTextEdit->deleteLater();
     tabs.erase(tabs.begin() + i);
     ui->tabWidget->removeTab(i);
     if (tabs.size() == 1) {
         ui->tabWidget->setTabsClosable(false);
     }
+}
+
+void MainWindow::on_actionSelect_All_triggered()
+{
+    tabs[currentTab()]->editor->selectAll();
 }
 
 
