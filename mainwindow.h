ef7b2f7a9f2b021b4731e43bc667c7421f97370d
polinatolmach <polina.tolmach@gmail.com>
2015-06-04 05:53:32 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -7,35 +7,9 @@
 #include <QMainWindow>
 #include <QtCore>
 #include <QtGui>
-#include <QPlainTextEdit>
 #include <QToolBar>
 #include <QVector>
-#include <QShortcut>
 #include <QString>
-
-#include <iostream>
-#include <QFile>
-#include <QFileDialog>
-#include <QIcon>
-#include <QIODevice>
-#include <QMessageBox>
-#include <QRegExp>
-#include <QShortcut>
-#include <QTextEdit>
-#include <QTextStream>
-#include <QToolButton>
-#include <QWidget>
-#include <string>
-#include <QPropertyAnimation>
-#include <QPushButton>
-#include <QTextCursor>
-#include <QDialog>
- class QAbstractItemModel;
- class QComboBox;
- class QCompleter;
- class QLabel;
- class QLineEdit;
- class QProgressBar;
 
 namespace Ui {
 class MainWindow;
@@ -50,18 +24,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-    QString textUnderCursor() const;
+    QString currentText() const;
 
 protected:
 
-
 private slots:
 
-    void switchToPrevTab();
-    void switchToNextTab();
-
     void makeNewTab();
-    void deleteTab(int);
+    void deleteTab(int tab_number);
     void deleteCurrentTab();
 
     void on_actionNew_triggered();
@@ -78,21 +48,21 @@ private slots:
     void on_actionNew_tab_triggered();
     void on_actionSelect_All_triggered();
 
+    void switchToPrevTab();
+    void switchToNextTab();
+
 private:
-    void displaySettingsDialog();
+
+    void setupEditor();
     void saveFileAs();
     void saveFile();
     void openFile();
-
-    void setupEditor();
-    QString extractFilename(QString path);
-    int currentTab();
-    int lightfactor;
     void setTabName(unsigned int index, QString text);
 
-    Ui::MainWindow *ui;
-    QShortcut *shortcut;
+    QString extractFilename(QString path);
+    int currentTab();
 
+    Ui::MainWindow *ui;
     struct Tab {
         int number;
         QString path;
@@ -108,9 +78,6 @@ private:
     QVector<Tab*> tabs;
 };
 
-
-#endif // MAINWINDOW_H
-
-
 #endif // MAINWINDOW_H
 \
+
