0eb895494ce2bc276af76ce0e877760d773fe842
polinatolmach <polina.tolmach@gmail.com>
2015-06-04 05:52:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include "mainwindow.h"
-#include "syntaxlighter.h"
+
 #include <QtGui>
 #include <QApplication>
-#include <iostream>
-#include <cstdlib>
 
-#include "syntaxlighter.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication pEdit(argc, argv);
     MainWindow window;
     window.resize(640, 512);
     window.show();
 
-    return a.exec();
+    return pEdit.exec();
 }
